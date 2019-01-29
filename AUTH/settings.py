e8cd9017cc1fc2b3ebf53f44248f7a447b1359cf
heartbeat007 <ornobtanvir.git@gmail.com>
2019-01-29 05:36:07 -0500

    initial commit

--- AUTH/settings.py
+++ AUTH/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 import os
-
+import django_heroku
+django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+# location where you will store your static files like bootstrap
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
+# location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn heartbeatdjango.wsgi
